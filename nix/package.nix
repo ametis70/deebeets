30ebbdf1ab8f679e532d3ad7803d703eb4a4f3ec
Ian Mancini <ianmethyst@gmail.com>
2026-08-01 01:50:20 -0300

    feat(nix): add flake

---
+++ nix/package.nix
@@ -0,0 +1,11 @@
+{ pkgs }:
+
+pkgs.buildGoModule {
+  pname = "deebeets";
+  version = "0.1.0";
+  src = ./..;
+
+  # Run `nix build` once with the placeholder below, then replace with the
+  # hash printed in the error message.
+  vendorHash = pkgs.lib.fakeHash;
+}
